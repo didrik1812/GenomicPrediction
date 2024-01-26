75b442844b09d39dbe1db25ddcbdff830073ad13
didrik1812 <didrik.sand@gmail.com>
2024-01-26 14:14:17 +0100

    training finished

--- src/visualization/island_diff.py
+++ src/visualization/island_diff.py
@@ -17,7 +17,7 @@ from plotnine import (
 
 PROJECT_DIR = Path(__file__).resolve().parents[2]
 
-def viz_between_outer_and_inner(df:pd.DataFrame, phenotype:str):
+def viz_between_outer_and_inner(df:pd.DataFrame, pheno:str):
     outer_idx = df.index[df.hatchisland.isin([22,23,24])]
     vgroup = np.vectorize(lambda x: "outer" if x in outer_idx else "inner")
     df["island_group"] = vgroup(df.index)
@@ -25,11 +25,11 @@ def viz_between_outer_and_inner(df:pd.DataFrame, phenotype:str):
     month_dict = {months[i]:str(i) for i in range(len(months))}
     vmonth = np.vectorize(lambda x: month_dict[x])
     df["month_group"] = vmonth(df.month)
-    p = ggplot(df, aes(x="month_group", y=phenotype)) +\
+    p = ggplot(df, aes(x="month_group", y ="pheno")) +\
             geom_boxplot()+\
             theme_bw()+\
             facet_grid(".~island_group")
-    p.save(PROJECT_DIR/"reports"/"figures"/f"outer_inner_comp_{phenotype}.pdf")
+    p.save(PROJECT_DIR/"reports"/"figures"/f"outer_inner_comp_{pheno}.pdf")
     
 
 def main():
